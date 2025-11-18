55245ada8da8c6a55716843dec919447eef19f4a
Konrad <kszatanek91@gmail.com>
2025-11-18 22:00:47 +0100

    refactor the first table and create the second one

--- wsb_db_7g.sql
+++ wsb_db_7g.sql
@@ -21,21 +21,52 @@ SET
 
 USE wsb_db;
 
-DROP TABLE ID EXISTS 'university';
+DROP TABLE IF EXISTS `university`;
 
-CREEATE TABLE 'university' (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(300) UNIQUE NOT NULL,
-    address VARCHAR(300) NOT NULL,
-    zip_code VARCHAR(20) NOT NULL,
-    city VARCHAR(100) NOT NULL,
-    phone VARCHAR(20) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL, --main_email
-    website VARCHAR(100) UNIQUE NOT NULL,
-    rector_name VARCHAR(50) NOT NULL,
-    rector_surname VARCHAR(50) NOT NULL,
-    dean_name VARCHAR(50) NOT NULL,
-    dean_surname VARCHAR(50) NOT NULL,
-    secretariat_phone VARCHAR(20) NOT NULL,
-    secretariat_email VARCHAR(100) UNIQUE NOT NULL,
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+CREATE TABLE
+    `university` (
+        `id` INT AUTO_INCREMENT PRIMARY KEY,
+        `name` VARCHAR(300) UNIQUE NOT NULL,
+        `address` VARCHAR(300) NOT NULL,
+        `zip_code` VARCHAR(20) NOT NULL,
+        `city` VARCHAR(100) NOT NULL,
+        `phone` VARCHAR(20) NOT NULL,
+        `email` VARCHAR(100) UNIQUE NOT NULL, --main_email
+        `website` VARCHAR(100) UNIQUE NOT NULL,
+        `rector_name` VARCHAR(50) NOT NULL,
+        `rector_surname` VARCHAR(50) NOT NULL,
+        `dean_name` VARCHAR(50) NOT NULL,
+        `dean_surname` VARCHAR(50) NOT NULL,
+        `secretariat_phone` VARCHAR(20) NOT NULL,
+        `secretariat_email` VARCHAR(100) UNIQUE NOT NULL
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+
+-- -- -------------------------------------------------------------------------------
+DROP TABLE IF EXISTS `teaching_staff`;
+
+CREATE TABLE
+    `teaching_staff` (
+        `id` INT AUTO_INCREMENT PRIMARY KEY,
+        `name` VARCHAR(50) NOT NULL,
+        `surname` VARCHAR(50) NOT NULL,
+        `sex` ENUM ('unknown', 'male', 'female', 'other') DEFAULT 'unknown',
+        `pesel` VARCHAR(11) UNIQUE NOT NULL,
+        `email` VARCHAR(100) UNIQUE NOT NULL,
+        `phone` VARCHAR(20) NOT NULL,
+        `street` VARCHAR(100) NOT NULL,
+        `house_number` VARCHAR(10) NOT NULL,
+        `apartment_number` VARCHAR(10) DEFAULT NULL,
+        `zip_code` VARCHAR(20) NOT NULL,
+        `city` VARCHAR(100) NOT NULL,
+        `hire_date` DATE NOT NULL,
+        `termination_date` DATE DEFAULT NULL,
+        `account_number` VARCHAR(34) UNIQUE NOT NULL,
+        `degree` ENUM (
+            'engineer',
+            'master',
+            'doctor',
+            'professor',
+            'other'
+        ) DEFAULT 'other' `university_id` INT NOT NULL, -- i'm not sure
+        FOREIGN KEY (`university_id`) REFERENCES `university` (`id`),
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;

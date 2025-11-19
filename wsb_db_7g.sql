ba2cba74a42c658f7dd6ce94dd39a3834f8ba621
Konrad <kszatanek91@gmail.com>
2025-11-19 23:14:39 +0100

    estetic refactor

--- wsb_db_7g.sql
+++ wsb_db_7g.sql
@@ -46,7 +46,6 @@ CREATE TABLE
         UNIQUE KEY `university_secretariat_email_uq` (`secretariat_email`)
     ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
--- -- -------------------------------------------------------------------------------
 DROP TABLE IF EXISTS `lecturers`;
 
 CREATE TABLE

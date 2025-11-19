69212b55af5b2a530076dd824580604eb6cc4003
Konrad <kszatanek91@gmail.com>
2025-11-19 22:37:05 +0100

    refactor about unique keys

--- wsb_db_7g.sql
+++ wsb_db_7g.sql
@@ -26,19 +26,23 @@ DROP TABLE IF EXISTS `university`;
 CREATE TABLE
     `university` (
         `id` INT AUTO_INCREMENT PRIMARY KEY,
-        `name` VARCHAR(300) UNIQUE NOT NULL,
+        `name` VARCHAR(300) NOT NULL,
         `address` VARCHAR(300) NOT NULL,
         `zip_code` VARCHAR(20) NOT NULL,
         `city` VARCHAR(100) NOT NULL,
         `phone` VARCHAR(20) NOT NULL,
-        `email` VARCHAR(100) UNIQUE NOT NULL, --main_email
-        `website` VARCHAR(100) UNIQUE NOT NULL,
+        `email` VARCHAR(100) NOT NULL, --main_email
+        `website` VARCHAR(100) NOT NULL,
         `rector_name` VARCHAR(50) NOT NULL,
         `rector_surname` VARCHAR(50) NOT NULL,
         `dean_name` VARCHAR(50) NOT NULL,
         `dean_surname` VARCHAR(50) NOT NULL,
         `secretariat_phone` VARCHAR(20) NOT NULL,
-        `secretariat_email` VARCHAR(100) UNIQUE NOT NULL
+        `secretariat_email` VARCHAR(100) NOT NULL,
+        UNIQUE KEY `university_name_uq` (`name`),
+        UNIQUE KEY `university_email_uq` (`email`),
+        UNIQUE KEY `university_website_uq` (`website`),
+        UNIQUE KEY `university_secretariat_email_uq` (`secretariat_email`)
     ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 -- -- -------------------------------------------------------------------------------
@@ -50,8 +54,8 @@ CREATE TABLE
         `name` VARCHAR(50) NOT NULL,
         `surname` VARCHAR(50) NOT NULL,
         `sex` ENUM ('unknown', 'male', 'female', 'other') DEFAULT 'unknown',
-        `pesel` VARCHAR(11) UNIQUE NOT NULL,
-        `email` VARCHAR(100) UNIQUE NOT NULL,
+        `pesel` VARCHAR(11) NOT NULL,
+        `email` VARCHAR(100) NOT NULL,
         `phone` VARCHAR(20) NOT NULL,
         `street` VARCHAR(100) NOT NULL,
         `house_number` VARCHAR(10) NOT NULL,
@@ -60,15 +64,19 @@ CREATE TABLE
         `city` VARCHAR(100) NOT NULL,
         `hire_date` DATE NOT NULL,
         `termination_date` DATE DEFAULT NULL,
-        `account_number` VARCHAR(34) UNIQUE NOT NULL,
+        `account_number` VARCHAR(34) NOT NULL,
         `degree` ENUM (
             'engineer',
             'master',
             'doctor',
             'professor',
             'other'
-        ) DEFAULT 'other' `university_id` INT NOT NULL, -- i'm not sure
-        FOREIGN KEY (`university_id`) REFERENCES `university` (`id`),
+        ) DEFAULT 'other',
+        UNIQUE KEY `teaching_staff_email_uq` (`email`),
+        UNIQUE KEY `teaching_staff_pesel_uq` (`pesel`),
+        UNIQUE KEY `teaching_staff_account_number_uq` (`account_number`),
+        -- `university_id` INT NOT NULL, -- i'm not sure
+        -- FOREIGN KEY (`university_id`) REFERENCES `university` (`id`),
     ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS `courses`;
@@ -77,9 +85,11 @@ CREATE TABLE
     `courses` (
         `id` INT AUTO_INCREMENT PRIMARY KEY,
         `name` VARCHAR(200) NOT NULL,
-        `code` VARCHAR(20) UNIQUE NOT NULL,
+        `code` VARCHAR(20) NOT NULL,
         `ects` INT NOT NULL,
         `description` TEXT DEFAULT NULL,
+        UNIQUE KEY `courses_name_uq` (`name`),
+        UNIQUE KEY `courses_code_uq` (`code`),
     ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS `students`;
@@ -87,12 +97,14 @@ DROP TABLE IF EXISTS `students`;
 CREATE TABLE
     `students` (
         `id` INT AUTO_INCREMENT PRIMARY KEY,
-        'number_index' VARCHAR(20) UNIQUE NOT NULL,
+        'number_index' VARCHAR(20) NOT NULL,
         `name` VARCHAR(50) NOT NULL,
         `surname` VARCHAR(50) NOT NULL,
         `sex` ENUM ('unknown', 'male', 'female', 'other') DEFAULT 'unknown',
-        `pesel` VARCHAR(11) UNIQUE NOT NULL,
+        `pesel` VARCHAR(11) NOT NULL,
         'birth_date' DATE NOT NULL,
+        UNIQUE KEY `students_number_index_uq` (`number_index`),
+        UNIQUE KEY `students_pesel_uq` (`pesel`),
     ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS `groups`;

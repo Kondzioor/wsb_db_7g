05d0d0c8390ef8bff6e661b04ce3290bdf72dc7d
Konrad <kszatanek91@gmail.com>
2025-11-20 20:08:53 +0100

    refactor the rest of relations and create table student_groups

--- wsb_db_7g.sql
+++ wsb_db_7g.sql
@@ -72,10 +72,14 @@ CREATE TABLE
             'professor',
             'other'
         ) DEFAULT 'other',
+        `university_id` INT unsigned NOT NULL,
         PRIMARY KEY (`id`),
         UNIQUE KEY `lecturers_email_uq` (`email`),
         UNIQUE KEY `lecturers_pesel_uq` (`pesel`),
-        UNIQUE KEY `lecturers_account_number_uq` (`account_number`) CONSTRAINT `lecturers_university_fk` FOREIGN KEY (`university_id`) REFERENCES `university` (`id`)
+        UNIQUE KEY `lecturers_account_number_uq` (`account_number`),
+        CONSTRAINT `lecturers_university_fk` FOREIGN KEY (`university_id`) REFERENCES `university` (`id`),
+        -- ON UPDATE CASCADE
+        -- ON DELETE RESTRICT??
     ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS `courses`;
@@ -85,11 +89,13 @@ CREATE TABLE
         `id` INT unsigned NOT NULL AUTO_INCREMENT,
         `name` VARCHAR(200) NOT NULL,
         `code` VARCHAR(20) NOT NULL,
-        `ects` INT NOT NULL,
+        `ects` TINYINT unsigned NOT NULL, -- chceck(ects > 0 and ects <= 30)   decimal(2,0)?
         `description` TEXT DEFAULT NULL,
+        `university_id` INT unsigned NOT NULL,
         PRIMARY KEY (`id`),
         UNIQUE KEY `courses_name_uq` (`name`),
-        UNIQUE KEY `courses_code_uq` (`code`)
+        UNIQUE KEY `courses_code_uq` (`code`),
+        CONSTRAINT `courses_university_fk` FOREIGN KEY (`university_id`) REFERENCES `university` (`id`) --
     ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS `students`;
@@ -101,11 +107,12 @@ CREATE TABLE
         `name` VARCHAR(50) NOT NULL,
         `surname` VARCHAR(50) NOT NULL,
         `sex` ENUM ('unknown', 'male', 'female', 'other') DEFAULT 'unknown',
-        `pesel` VARCHAR(11) NOT NULL,
+        `pesel` VARCHAR(11) NOT NULL, -- BLOB ?
         `birth_date` DATE NOT NULL,
+        `university_id` INT unsigned NOT NULL,
         PRIMARY KEY (`id`),
         UNIQUE KEY `students_number_index_uq` (`number_index`),
-        UNIQUE KEY `students_pesel_uq` (`pesel`)
+        UNIQUE KEY `students_pesel_uq` (`pesel`) CONSTRAINT `students_university_fk` FOREIGN KEY (`university_id`) REFERENCES `university` (`id`) --
     ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS `groups`;
@@ -117,8 +124,25 @@ CREATE TABLE
         `year` INT NOT NULL,
         `lecturer_id` INT NOT NULL,
         `specialization` VARCHAR(100) NOT NULL,
+        `university_id` INT unsigned NOT NULL,
         PRIMARY KEY (`id`),
-        UNIQUE KEY `group_uq` (`name`, `year`)
+        UNIQUE KEY `group_uq` (`name`, `year`) -- ?
+        CONSTRAINT `groups_lecturer_fk` FOREIGN KEY (`lecturer_id`) REFERENCES `lecturers` (`id`), --
+        CONSTRAINT `groups_university_fk` FOREIGN KEY (`university_id`) REFERENCES `university` (`id`) --
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+
+DROP TABLE IF EXISTS `student_groups`;
+
+CREATE TABLE
+    `student_groups` (
+        `id` INT unsigned NOT NULL AUTO_INCREMENT,
+        `student_id` INT NOT NULL,
+        `group_id` INT NOT NULL,
+        `enroll_date` DATE NOT NULL,
+        PRIMARY KEY (`id`),
+        UNIQUE KEY `student_group_uq` (`student_id`, `group_id`),
+        CONSTRAINT `student_groups_student_fk` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`),
+        CONSTRAINT `student_groups_group_fk` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`)
     ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS `partial_grades`;
